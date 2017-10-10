8f56868d32ec272759d393242a2684ac7a00b68d
余锋 <690174435@qq.com>
2017-10-10 10:42:08 +0800

    Update SealView.java

--- sealviewlib/src/main/java/com/yufeng/sealview/lib/SealView.java
+++ sealviewlib/src/main/java/com/yufeng/sealview/lib/SealView.java
@@ -18,21 +18,47 @@ import android.view.View;
 
 public class SealView extends View {
 
-    private int radius;
-    private int color;
-
-    private int textSize;
-    private String textFirst;
-    private String textSecond;
-
-    private int viewWidth;
+  private int viewWidth;
     private int viewHeight;
 
     private Paint paint;
     private Path pathString;
     private RectF rectF;
 
+    // -- 定制
+    private int radius;
+    private int color;
     private int rotate;
+
+    private float firstTextSize;
+    private float secondTextSize;
+    private String textFirst;
+    private String textSecond;
+
+    private int bigOvalWidth = 4;  // 大圆宽度
+
+    private int smallOvalWidth = 1; // 小圆宽度
+
+    private int bigSmallMargin = 10; //大圆小圆的间距
+
+    private float starWidthMultiple = 1; //星星大小
+    private float firstTextMarginMultiple = 1; //字体位置
+    private float secondTextMarginMultiple = 1; 
+
+    private Typeface firstTextTypeface;
+    private Typeface secondTypeface;
+
+    private String firstTextPath;
+    private String secondTextPath;
+
+    private boolean showFirstBold;
+    private boolean showSecondBold;
+    private boolean showSecondUnderline;
+    private boolean showSecondDeleteline;
+
+    private int secondTextAlign = 1;
+
+    float tempR;
 
     public SealView(Context context) {
         this(context, null);
@@ -47,7 +73,8 @@ public class SealView extends View {
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.SealView);
         radius = array.getDimensionPixelOffset(R.styleable.SealView_seal_radius, 90);
         color = array.getColor(R.styleable.SealView_seal_color, Color.RED);
-        textSize = array.getDimensionPixelSize(R.styleable.SealView_seal_textSize, 18);
+        firstTextSize = array.getDimensionPixelSize(R.styleable.SealView_seal_textSize, 18);
+        secondTextSize = firstTextSize - 4;
         textFirst = array.getString(R.styleable.SealView_seal_textFirst);
         textSecond = array.getString(R.styleable.SealView_seal_textSecond);
         rotate = array.getInt(R.styleable.SealView_seal_rotate, 0);
@@ -58,6 +85,17 @@ public class SealView extends View {
         pathString = new Path();
         rectF = new RectF();
 
+        tempR = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                300, context.getResources().getDisplayMetrics()) / 3f;
+
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
     }
 
     @Override
@@ -65,6 +103,9 @@ public class SealView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         viewWidth = w;
         viewHeight = h;
+
+        //绑定radius和ViewWidth
+        radius = viewWidth / 3;
     }
 
     @Override
@@ -81,15 +122,18 @@ public class SealView extends View {
 
         //绘制外围大圆
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(4);
+        paint.setStrokeWidth(bigOvalWidth * (radius / tempR));
         canvas.drawCircle(centerX, centerY, radius, paint);
 
         //绘制小圆
-        paint.setStrokeWidth(1);
-        canvas.drawCircle(centerX, centerY, radius - 10, paint);
+        if (smallOvalWidth > 0) {
+            paint.setStrokeWidth(smallOvalWidth * (radius / tempR));
+            canvas.drawCircle(centerX, centerY, radius - (bigSmallMargin * (radius / tempR)), paint);
+        }
 
         //绘制五角星
-        int width = radius * 3;
+        float width = radius * 3 * starWidthMultiple;
+
 
         float outR = width / 2 / 5;
         float inR = outR * sin(18) / sin(180 - 36 - 18);
@@ -103,18 +147,31 @@ public class SealView extends View {
         canvas.drawPath(path, paint);
         canvas.restore();
 
-        paint.setTextSize(textSize);
         paint.setStyle(Paint.Style.FILL);
 
         float sweepAngle;
         float startAngle;
-        int rString = radius - 30;
+        int rString = (int) (radius * 0.8 * firstTextMarginMultiple);
+        rectF.set(centerX - rString, centerY - rString, centerX + rString, centerY + rString);
 
         //绘制上半部分字符
         if (!TextUtils.isEmpty(textFirst)) {
+            paint.setTextSize(firstTextSize * (radius / tempR));
+            paint.setFakeBoldText(showFirstBold);
+//            paint.setUnderlineText(showFirstUnderline);
+//            paint.setStrikeThruText(showFirstDeleteline);
+
+            paint.setTypeface(Typeface.DEFAULT);
+            if (!TextUtils.isEmpty(firstTextPath)) {
+                firstTextTypeface = getTypefaceFromPath(firstTextPath);
+            }
+
+            if (firstTextTypeface != null) {
+                paint.setTypeface(firstTextTypeface);
+            }
+
             float textW = paint.measureText(textFirst);
 
-            rectF.set(centerX - rString, centerY - rString, centerX + rString, centerY + rString);
             pathString.reset();
             sweepAngle = 180 * textW / (3.1415f * rString);
             startAngle = 90 - sweepAngle / 2;
@@ -126,20 +183,62 @@ public class SealView extends View {
 
         //绘制下半部分字符
         if (!TextUtils.isEmpty(textSecond)) {
-            paint.setTextSize(textSize - 2);
+            paint.setTextSize(secondTextSize * (radius / tempR));
+            paint.setFakeBoldText(showSecondBold);
+            paint.setUnderlineText(showSecondUnderline);
+            paint.setStrikeThruText(showSecondDeleteline);
+
+            paint.setTypeface(Typeface.DEFAULT);
+            if (!TextUtils.isEmpty(secondTextPath)) {
+                secondTypeface = getTypefaceFromPath(secondTextPath);
+            }
+
+            if (secondTypeface != null) {
+                paint.setTypeface(secondTypeface);
+            }
+
             float textWidth = paint.measureText(textSecond);
 
-            sweepAngle = 180 * textWidth / (3.1415f * rString);
-            startAngle = 90 + sweepAngle / 2;
-            pathString.reset();
-            pathString.arcTo(rectF, startAngle, -sweepAngle, true);
+            if (secondTextAlign == 1) {
+                //弧
+                rString = (int) (radius * 0.8 * secondTextMarginMultiple);
+                rectF.set(centerX - rString, centerY - rString, centerX + rString, centerY + rString);
 
-            canvas.drawTextOnPath(textSecond, pathString, 0, 0, paint);
+                sweepAngle = 180 * textWidth / (3.1415f * rString);
+                startAngle = 90 + sweepAngle / 2;
+                pathString.reset();
+                pathString.arcTo(rectF, startAngle, -sweepAngle, true);
+
+                canvas.drawTextOnPath(textSecond, pathString, 0, 0, paint);
+            } else {
+                //横线
+                paint.setTextAlign(Paint.Align.CENTER);
+                Paint.FontMetrics fm = paint.getFontMetrics();
+                float textHeight = (int) Math.ceil(fm.descent - fm.ascent);
+
+                float textAlign = (float) (radius * 0.5 * secondTextMarginMultiple);
+
+                canvas.drawText(textSecond, centerX, centerY + textAlign + textHeight * 0.5f, paint);
+
+                paint.setTextAlign(Paint.Align.LEFT);
+
+            }
         }
 
         canvas.restore();
 
     }
+
+    public static Typeface getTypefaceFromPath(String textTypefacePath) {
+        File file = new File(textTypefacePath);
+
+        if (!file.exists() || file.isDirectory()) {
+            return null;
+        }
+
+        return Typeface.createFromFile(file);
+    }
+
 
     private Path getCompletePath(float outR, float inR) {
         Path path = new Path();
@@ -166,5 +265,184 @@ public class SealView extends View {
 
     static float sin(int num) {
         return (float) Math.sin(num * Math.PI / 180);
+    }
+
+    public String getFirstTextPath() {
+        return firstTextPath;
+    }
+
+    public void setFirstTextPath(String firstTextPath) {
+        this.firstTextPath = firstTextPath;
+    }
+
+    public String getSecondTextPath() {
+        return secondTextPath;
+    }
+
+    public void setSecondTextPath(String secondTextPath) {
+        this.secondTextPath = secondTextPath;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public float getFirstTextSize() {
+        return firstTextSize;
+    }
+
+    public void setFirstTextSize(int textSize) {
+        //SP
+        float newSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, textSize, getContext().getResources().getDisplayMetrics());
+        this.firstTextSize = newSize;
+    }
+
+    public String getTextFirst() {
+        return textFirst;
+    }
+
+    public void setTextFirst(String textFirst) {
+        this.textFirst = textFirst;
+    }
+
+    public String getTextSecond() {
+        return textSecond;
+    }
+
+    public void setTextSecond(String textSecond) {
+        this.textSecond = textSecond;
+    }
+
+    public int getRotate() {
+        return rotate;
+    }
+
+    public void setRotate(int rotate) {
+        this.rotate = rotate;
+    }
+
+    public void setStarWidthMultiple(float starWidthMultiple) {
+        this.starWidthMultiple = starWidthMultiple;
+    }
+
+    public float getStarWidthMultiple() {
+        return starWidthMultiple;
+    }
+
+    public void setFirstTextTypeface(Typeface firstTextTypeface) {
+        this.firstTextTypeface = firstTextTypeface;
+    }
+
+    public Typeface getFirstTextTypeface() {
+        return firstTextTypeface;
+    }
+
+    public void setSecondTypeface(Typeface secondTypeface) {
+        this.secondTypeface = secondTypeface;
+    }
+
+    public Typeface getSecondTypeface() {
+        return secondTypeface;
+    }
+
+    public float getSecondTextMarginMultiple() {
+        return secondTextMarginMultiple;
+    }
+
+    public void setSecondTextMarginMultiple(float secondTextMarginMultiple) {
+        this.secondTextMarginMultiple = secondTextMarginMultiple;
+    }
+
+    public int getSecondTextAlign() {
+        return secondTextAlign;
+    }
+
+    public void setSecondTextAlign(int secondTextAlign) {
+        this.secondTextAlign = secondTextAlign;
+    }
+
+    public float getSecondTextSize() {
+        return secondTextSize;
+    }
+
+    public void setSecondTextSize(float secondTextSize) {
+        float newSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, secondTextSize, getContext().getResources().getDisplayMetrics());
+        this.secondTextSize = newSize;
+    }
+
+    public boolean isShowSecondBold() {
+        return showSecondBold;
+    }
+
+    public void setShowSecondBold(boolean showSecondBold) {
+        this.showSecondBold = showSecondBold;
+    }
+
+    public boolean isShowSecondUnderline() {
+        return showSecondUnderline;
+    }
+
+    public void setShowSecondUnderline(boolean showSecondUnderline) {
+        this.showSecondUnderline = showSecondUnderline;
+    }
+
+    public boolean isShowSecondDeleteline() {
+        return showSecondDeleteline;
+    }
+
+    public void setShowSecondDeleteline(boolean showSecondDeleteline) {
+        this.showSecondDeleteline = showSecondDeleteline;
+    }
+
+    public boolean getShowFirstBold() {
+        return showFirstBold;
+    }
+
+    public void setShowFirstBold(boolean showFirstBold) {
+        this.showFirstBold = showFirstBold;
+    }
+
+    public float getFirstTextMarginMultiple() {
+        return firstTextMarginMultiple;
+    }
+
+    public void setFirstTextMarginMultiple(float firstTextMarginMultiple) {
+        this.firstTextMarginMultiple = firstTextMarginMultiple;
+    }
+
+    public int getBigSmallMargin() {
+        return bigSmallMargin;
+    }
+
+    public void setBigSmallMargin(int bigSmallMargin) {
+        this.bigSmallMargin = bigSmallMargin;
+    }
+
+    public int getBigOvalWidth() {
+        return bigOvalWidth;
+    }
+
+    public void setBigOvalWidth(int bigOvalWidth) {
+        this.bigOvalWidth = bigOvalWidth;
+    }
+
+    public int getSmallOvalWidth() {
+        return smallOvalWidth;
+    }
+
+    public void setSmallOvalWidth(int smallOvalWidth) {
+        this.smallOvalWidth = smallOvalWidth;
     }
 }
